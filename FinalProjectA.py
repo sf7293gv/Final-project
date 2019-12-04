7c787541d9fca757ffb28bfa028f0e34b19406c9
bobobobo <2>
2019-12-04 12:03:24 -0600

    added the taco recipe link to the project and the taco image too

--- FinalProjectA.py
+++ FinalProjectA.py
@@ -1,4 +1,8 @@
 import requests # imported requests
 import docx # imported python-docs
-import pillow # installed and imported the pillow library
+from PIL import Image, ImageDraw, ImageFont  # imported these libraries to be able wo work on the image
+# installed and imported the pillow library
+
+taco_recipe_urldata = requests.get('https://taco-1150.herokuapp.com/random/?full_taco=true')
+     #  using the requests library to open the url link and get the dictionary that we are gonna use
 
