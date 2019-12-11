e5fde4b544572681b2f5e40460d252973642f98f
bobobobo <2>
2019-12-11 11:59:39 -0600

    I've edited the image size, and the text size. And I have added a for loop that I will comment in the next push

--- FinalProjectA.py
+++ FinalProjectA.py
@@ -5,11 +5,12 @@ from PIL import Image, ImageDraw, ImageFont  # imported these libraries to be ab
 
 
 # Working on the image down here
+# iimage is the new edited image
 image = Image.open('tacorecipe.jpg')  # the pic that I am gonna use (Taco image)
-image.thumbnail((800, 800))  # Because the image is big, I resized it to 800/800
+image.thumbnail((700, 700))  # Because the image is big, I resized it to 800/800
 iimage = ImageDraw.Draw(image)  # I've used the Imagedraw library to be able to put text on the image
 font = ImageFont.truetype('DejaVuSans.ttf', 25)  # using a database for the fonts
-iimage.text([10, 475], 'Random Taco Cookbook', fill='black', font=font)  # drawing text on the image
+iimage.text([50, 400], 'Random Taco Cookbook', fill='black', font=font)  # drawing text on the image
 image.show()  # shows the new edited version of the image
 image.save('usingInWord.jpeg')  # This statement saves the image so we can be able to use later in the project
 
@@ -17,8 +18,8 @@ taco3 = []  # a list to stole the dictionaries in
 
 for i in range(3):  # I've used a for loop here so I can get the result from url 3 times (the dictionaries)
     try: # I've added the try and except part just if there was no internet
-        taco_recipe_urldata = requests.get('https://taco-1150.herokuapp.com/random/?full_taco=true').json()   # using the requests library to open the url link and get the dictionary that we are gonna use
-        taco3.append(taco_recipe_urldata)  # adds the results to the list created earlier
+        taco_recipe_url_data = requests.get('https://taco-1150.herokuapp.com/random/?full_taco=true').json()   # using the requests library to open the url link and get the dictionary that we are gonna use
+        taco3.append(taco_recipe_url_data)  # adds the results to the list created earlier
         print(taco3)
     except:
         print('You are not online')  # prints if there is no internet
@@ -27,10 +28,21 @@ word_document = docx.Document()  # This will open a new word document
 
 word_document.add_paragraph('Random Taco Cookbook', 'Title')  # This will add a heading in the new word document
 
-word_document.add_picture('usingInWord.jpeg')  # This statement is gonna use the save image and add it to theNewDocument
+word_document.add_picture('usingInWord.jpeg')  # This statement is gonna use the saved image, and add it to theNewDocument
 
 word_document.add_paragraph('Tai\'s Captures')  # This is gonna add a new paragraph with a name in it
 
 word_document.add_paragraph('https://taco-1150.herokuapp.com/random/?full_taco=true')  # This is gonna add a new paragraph with the taco url in it
 
 word_document.add_paragraph('Mohammad Zabaneh')  # This is gonna add a new paragraph with my name in it
+
+# word_document.add_page_break()
+
+for i in taco3:
+    word_document.add_paragraph(taco_recipe_url_data["seasoning"])
+    word_document.add_paragraph(taco_recipe_url_data["condiment"])
+    word_document.add_paragraph(taco_recipe_url_data["mixin"])
+    word_document.add_paragraph(taco_recipe_url_data["base_layer"])
+    word_document.add_paragraph(taco_recipe_url_data["shell"])
+
+
